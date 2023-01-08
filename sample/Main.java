c88f4698e224f6c308c84fb091571484b42b0669
shun0803 <121324810+shun0803@users.noreply.github.com>
2023-01-08 03:22:30 +0000

    aa

---
+++ sample/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(12345+23456);
+		System.out.println(12345*6789);
+		System.out.println(100.0 / 15);
+		System.out.println(100 % 15);
+	}
+
+}
